7843ca843c519c23e1d1e4a0e208a4b20892de93
Nandhin98 <47237438+Nandhin98@users.noreply.github.com>
2019-07-09 15:25:26 +0530

    Create 31.py

---
+++ 31.py
@@ -0,0 +1,19 @@
+ import sys,string, math,itertools
+ s = input()
+ b1=0
+ n = len(s)
+ if s[0]== ')' :
+     printf("no")
+     sys.exit()
+ for i in range(0,n) :
+  if s[i] == '(' :
+         b1 += 1
+     if s[i] == ')' :
+          b1 -= 1
+     if b1<0 :
+         printf("no")
+         sys.exit()
+ if b1==0 :
+     print("yes")
+ else :
+     print("no")
